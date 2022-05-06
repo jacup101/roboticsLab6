434d045e3c9c6ee9cffc46986741be711047fa12
jacup101 <jacup105@gmail.com>
2022-05-05 19:53:53 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lab 6 - Automated Drones with Computer vision
+This is the github repository for my Lab 6 for Robotics, which seeks to create drones which can traverse the perimeter of a room using computer vision in place of sensors. Further details, beyond the scope of this readme, can be found in the included PDFs for both the pre-lab and the lab.
+
+## Part 1 - Normal Traversal
+This part of the lab deals with the drone in only 2 directions, the code can be found under part1.py. This currently does not actually have the code to go around the room, since that would be dangerous to test, but has the capability given the correct environment. A video demonstration can be found [here](https://www.youtube.com/watch?v=OqBKJ8uYz18).
+
+## Part 2 - Additional DoF
+This part o the lab attempts to add an additional degree of freedom, allowing the drone to decide to go up and down. The code can be found in part2.py, and again does not actually fly around the room, but has the capability with a couple of changes, given the correct environment. A video demonstration can be found [here](https://www.youtube.com/watch?v=ntEDFWZ8T-M).
---
+++ part1.py
@@ -0,0 +1,166 @@
+import tellopy
+import cv2
+import os
+import datetime
+import time
+import random
+import numpy as np
+pathToImg = "null"
+
+
+
+
+
+
+def handlePictureReceived(event, sender, data):
+    global pathToImg
+    # Create a file in ~/Pictures/ to receive image data from the drone.
+    path = '%s/Volumes/workplace/robotics/lab6/photos/tello-%s.jpeg' % (
+        os.getenv('HOME'),
+        datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S'))
+    with open(path, 'wb') as fd:
+        fd.write(data)
+    pathToImg = path
+
+
+def generateRandRGBTuple():
+    return (random.randint(0,255), random.randint(0, 255), random.randint(0, 255))
+
+
+def handle_contours(pathToImg, searchingFor, show):
+    img = cv2.imread(pathToImg)
+    # Get a grayscale of the image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Get params to create canny image
+    median = cv2.medianBlur(gray, 3)
+    v = np.median(gray)
+    sigma = .33
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    # Get edges using canny algo
+    edged = cv2.Canny(median, lower, upper)
+    # Attempt to close the image using morphology
+    se = np.ones((10,10), dtype='uint8')
+    image_close = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, se)
+    # Attempt to close any remaining gaps using a slight dilation
+    kernel = np.ones((5,5),np.uint8)
+    dilated= cv2.dilate(image_close, kernel, iterations = 2)
+
+    # Set up parameters for CV decisions
+    rectangleFound = False
+    maxArea = 40000
+    # Finding Contours
+    # Use a copy of the image e.g. edged.copy()
+    # since findContours alters the image
+
+    # RECTANGLES + TRIANGLES DETECTION
+    contours, hierarchy = cv2.findContours(dilated,
+        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+
+        area = cv2.contourArea(contour)
+        if area < 250.0:
+            pass
+        else:
+            approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+
+            randRGB = generateRandRGBTuple()
+
+            x = approx.ravel()[0]
+            y = approx.ravel()[1] - 5
+            if len(approx) == 3:
+                cv2.putText( img, "Triangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, randRGB )
+                cv2.drawContours(img, [approx], 0, randRGB, 5)
+
+            elif len(approx) >= 4 and len(approx) < 8:
+                # We have a rectangle
+                print("Rectangle with area: {0} detected".format(area))
+                cv2.putText(img, "rectangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, randRGB)
+                cv2.drawContours(img, [approx], 0, randRGB, 5)
+                if area > maxArea:
+                    rectangleFound = True
+            elif len(approx) > 8 and cv2.isContourConvex(approx):
+                print("Circle with area: {0} detected".format(area))
+                cv2.putText(img, "circle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, randRGB)
+            cv2.drawContours(img, [approx], 0, randRGB, 5)
+    if show:
+        cv2.imshow('shapes', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    if searchingFor in "rectangle":
+        return rectangleFound
+
+
+
+
+
+
+def initDrone(drone, waitTime):
+    drone.connect()
+    print("Connecting..")
+    drone.subscribe(drone.EVENT_FILE_RECEIVED, handlePictureReceived)
+    time.sleep(5)
+
+def takeoff(drone, down, waitTime):
+    drone.takeoff()
+    time.sleep(waitTime)
+    drone.down(down)
+    time.sleep(waitTime)
+
+def land(drone, waitTime):
+    drone.land()
+    time.sleep(waitTime)
+
+def flyForward(drone, distance, sleepTime):
+    drone.forward(distance)
+    time.sleep(sleepTime)
+
+def flyUpward(drone, distance, sleepTime):
+    drone.up(distance)
+    time.sleep(sleepTime)
+
+def rotateCCW(drone, angle, sleepTime):
+    drone.counter_clockwise(angle)
+    time.sleep(sleepTime)
+
+
+def processCV(drone, waitTime, searchingFor):
+    drone.take_picture()
+    time.sleep(waitTime)
+    return handle_contours(pathToImg, searchingFor, False)
+
+useDrone = True
+
+if useDrone:
+    drone = tellopy.Tello()
+    initDrone(drone, 10)
+
+    takeoff(drone, 30, 5)
+    #flyUpward(drone,10,5)
+    time.sleep(3)
+    numAllowedSteps = 3
+    steps = 0
+    while steps < numAllowedSteps:
+        flyForward(drone, 10, 5)
+        time.sleep(3)
+        searchResult = processCV(drone, 5, 'rectangle')
+        if searchResult:
+            rotateCCW(drone, 20, 3)
+        steps += 1
+
+
+
+
+    land(drone, 5)
+
+
+    drone.quit()
+    time.sleep(3)
+
+
+    time.sleep(3)
+else:
+    print("Not using drone...")
+    handle_contours("./photos/tello-2022-05-03_110613.jpeg", "rectangle", True)
